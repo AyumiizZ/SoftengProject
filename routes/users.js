6b9a70809c8f6872e91690d2ed1b7d0021d5f84a
worachai3 <worachai3@live.com>
2018-09-16 00:17:39 +0700

    Add freelance route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/freelance', function(req, res, next){
+  res.render('freelance');
+});
+
 module.exports = router;

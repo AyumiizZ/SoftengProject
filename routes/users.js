bbe8f95a3fecaa1db8ca213d83b2791383ba1b47
AyumiizZ <korrawitc@gmail.com>
2018-09-17 11:24:30 +0700

    :sparkles: add forget password

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register',function(req,res){
+router.get('/register', function (req, res) {
   res.render('register');
 });
 
-router.get('/login',function(req,res){
+router.post('/register', function (req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const confirm = req.body.confirm;
+  const isTick = req.body.agreement
+
+  console.log('click');
+  if(password == confirm && isTick == 'on'){
+    console.log('match');
+  }
+  else{
+    console.log('ERROR');
+  }
+  res.render('register')
+});
+
+router.get('/login', function (req, res) {
   res.render('login');
+});
+
+router.get('/resetPassword', function (req, res) {
+  res.render('resetPassword');
 });
 
 module.exports = router;

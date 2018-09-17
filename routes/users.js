81c78186a5ea3e53791a1627233deddc246e8b71
AyumiizZ <korrawitc@gmail.com>
2018-09-17 11:45:34 +0700

    revert

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
@@ -11,23 +10,4 @@ router.get('/freelance', function(req, res, next){
   res.render('freelance');
 });
 
-router.get('/register',function(req,res){
-  res.render('register');
-});
-
-router.get('/login',function(req,res){
-  res.render('login');
-});
-
 module.exports = router;
-=======
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
->>>>>>> fe11b50345c295d1ac3285ba607b5e25564edbbc

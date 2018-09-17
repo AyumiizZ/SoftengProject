41e7a260e66efeb8aa2a142f7f06f07cdf94c92a
morganmox <30286286+morganmox@users.noreply.github.com>
2018-09-17 11:11:22 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,26 @@
-require("dotenv").config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var session = require("express-session");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret:
-      process.env.SECRET_KEY ||
-      "\xd3\xae\x07z\xbd\xfb\xea\xa74\xc3g\x1bt,y\xe8?\xfb\xe0\x85\x0f'\x83\x8c",
-    resave: false,
-    saveUninitialized: true
-  })
-);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,11 +31,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/register',function(req,res){
-  res.render('register');
-});
-
-router.get('/login',function(req,res){
-  res.render('login');
-});
-
-router.get('/addannouncement',function(req,res){
-  res.render('addannouncement');
-});
-
 module.exports = router;

e43a0b9d8cd4393e2b719d3aadd8d8b04f2f796d
Sirapob Kangkan <morgan_mox@hotmail.com>
2018-10-05 20:20:07 +0700

    kuykuykuy

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const Job = require("../models/job");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,9 +13,11 @@ router.get("/addjob", function(req, res) {
 });
 
 router.post("/addjob", function(req, res, next) {
+  console.log(Job);
   console.log(req.body);
-  Jobs.query().insert(req.body);
   res.redirect("/");
-});
+  return Job.query().insert(req.body);
+  }
+);
 
 module.exports = router;

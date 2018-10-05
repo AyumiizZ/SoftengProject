c9667ad6c877029bdf0b8938ba24737c3b99e820
Rawissara <rawissara.r@ku.th>
2018-10-05 16:20:28 +0700

    Update profile

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
-const { sanitizeBody } = require("express-validator/filter");
+const {
+  check,
+  validationResult
+} = require("express-validator/check");
+const {
+  sanitizeBody
+} = require("express-validator/filter");
 const bcrypt = require("bcrypt");
 const _helpers = require("../auth/_helpers");
 const User = require("../models/user");
 const passport = require("passport");
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.redirect("/");
 });
 
-router.get("/register", function(req, res) {
+router.get("/register", function (req, res) {
   res.render("register");
 });
 
@@ -20,15 +25,15 @@ router.post(
   "/register",
   [
     check("email")
-      .isEmail()
-      .withMessage("E-mail not recognised."),
+    .isEmail()
+    .withMessage("E-mail not recognised."),
     check("password")
-      .isLength({
-        min: 8
-      })
-      .withMessage("Password must be longer than 8 characters.")
+    .isLength({
+      min: 8
+    })
+    .withMessage("Password must be longer than 8 characters.")
   ],
-  function(req, res) {
+  function (req, res) {
     const errors = validationResult(req).array();
     if (errors.length > 0) {
       res.send(errors.array);
@@ -51,7 +56,7 @@ router.post(
   }
 );
 
-router.get("/login", function(req, res) {
+router.get("/login", function (req, res) {
   if (req.user) {
     res.redirect("/");
   }
@@ -63,21 +68,21 @@ router.post(
   passport.authenticate("local", {
     failureRedirect: "login"
   }),
-  function(req, res, next) {
+  function (req, res, next) {
     res.redirect("/");
   }
 );
 
-router.get("/logout", function(req, res) {
+router.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/");
 });
 
-router.get("/resetPassword", function(req, res) {
+router.get("/resetPassword", function (req, res) {
   res.render("resetPassword");
 });
 
-router.post("/resetPassword", function(req, res) {
+router.post("/resetPassword", function (req, res) {
   const username = req.body.username;
   const answer = req.body.answer.toLowerCase();
 
@@ -95,19 +100,23 @@ router.post("/resetPassword", function(req, res) {
   res.render("resetPassword");
 });
 
-//<<<<<<< HEAD
 router.get("/:username", async function (req, res) {
   let user = await User.query().where('username', req.params.username).first();
-  res.render("profile", {user: user});
+  res.render("profile", {
+    user: user
+  });
 });
 
 router.get("/:username/edit", async function (req, res) {
   let user = await User.query().where('username', req.params.username).first();
-  res.render("editProfile", {user: user});
+  res.render("editProfile", {
+    user: user
+  });
 });
 
 router.post("/:username/edit", async function (req, res, next) {
   let user = await User.query().where('username', req.params.username).first();
+  let updatedUser = await User.query().updateAndFetchById(user.id, req.body);
   res.redirect("/users/"+user.username);
 });
 /*=======

a02de2c535f78bde5672030476f6bfa96bff2af3
AyumiizZ <korrawitc@gmail.com>
2018-10-06 21:29:08 +0700

    :sparkles: make js pass skills

--- routes/search.js
+++ routes/search.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // res.render('index', { title: 'Test' });
   res.redirect('/search/projects')
 });
 
-router.get('/projects', function(req, res, next) {
-  res.render('projects');
+router.get('/projects', function (req, res, next) {
+  var user_skills = ["PHP", "Python", "MySQL", "Linux", "JavaScript"]
+  res.render('projects', {
+    user_skills: user_skills
+  });
 });
 
 module.exports = router;

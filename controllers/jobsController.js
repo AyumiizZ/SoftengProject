feec525110c6ba807f0096f08c25820a86894ab3
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-10-07 00:22:15 +0700

    :sparkle: Saving job interests

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 const Job = require("../models/job");
+const JobInterest = require("../models/jobInterest");
 
 exports.view = async function(req, res, next) {
   console.log(req.params.jobId);
@@ -14,4 +15,23 @@ exports.interestedGet = async function(req, res, next) {
     .findById(req.params.jobId)
     .eager("[client, freelance]");
   res.render("jobs/interested", { job: job });
+};
+
+exports.interestedPost = async function(req, res, next) {
+  const currentUsername = req.user.username;
+  const jobId = req.params.jobId;
+  var data = {
+    user_username: currentUsername,
+    job_id: jobId,
+    message: req.body.message
+  };
+  try {
+    const newJobInterest = await JobInterest.query().insert(data);
+  } catch (err) {
+    var updatedJobInterest = await JobInterest.query().updateAndFetchById(
+      [jobId, currentUsername],
+      data
+    );
+  }
+  res.json(data);
 };
--- routes/job.js
+++ routes/job.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var jobsController = require("../controllers/jobsController");
+var authMiddleware = require("../middlewares/authMiddleware");
 
 const Job = require("../models/job");
 
@@ -9,7 +10,16 @@ router.get("/", function(req, res) {
 });
 
 router.get("/view/:jobId", jobsController.view);
-router.get("/interested/:jobId", jobsController.interestedGet);
+router.get(
+  "/interested/:jobId",
+  authMiddleware.isAuthenticated,
+  jobsController.interestedGet
+);
+router.post(
+  "/interested/:jobId",
+  authMiddleware.isAuthenticated,
+  jobsController.interestedPost
+);
 
 router.get("/add", function(req, res) {
   res.render("addjob");

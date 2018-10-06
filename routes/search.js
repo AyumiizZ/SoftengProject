281bd21db64b5bd261742137141188962ee3ccbd
AyumiizZ <korrawitc@gmail.com>
2018-10-06 15:59:18 +0700

    :sparkles: add box

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Test' });
-  res.redirect('/search/view_jobs')
+  res.redirect('/search/projects')
 });
 
 router.get('/projects', function(req, res, next) {

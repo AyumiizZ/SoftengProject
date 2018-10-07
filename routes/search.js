51e5ab352d2dbb47d6bbc229d907127c514ca9ec
AyumiizZ <korrawitc@gmail.com>
2018-10-07 19:03:34 +0700

    :sparkles: render from db

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-// const Job = require("../models/job")
+const Job = require("../models/job")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect('/search/projects')
 });
 
-router.get('/projects', function (req, res, next) {
+router.get('/projects', async function (req, res, next) {
   var user_skills = ["PHP", "Python", "MySQL", "Linux", "JavaScript"]
   var user_lang = ["Thai","English"]
-  // const jobs = await Job.query(); 
-  var jobs = [{id:1,job:'test',job_info:'Lorem',job_type:'Hourly',tag:'Python',price:500},{id:2,job:'test2',job_info:'Lorem2',job_type:'Fixed',tag:'PHP',price:7500}]
+  const jobs = await Job.query(); 
+  // var jobs = [{id:1,job:'test',job_info:'Lorem',job_type:'Hourly',tag:'Python',price:500},{id:2,job:'test2',job_info:'Lorem2',job_type:'Fixed',tag:'PHP',price:7500}]
   res.render('projects', {
     jobs: jobs,
     n_results: jobs.length,

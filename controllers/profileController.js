acf3e5e6ca5e943d9ba24059f1e6e26c47f043e1
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-10-24 13:52:33 +0700

    :sparkle: Objection-based relation definition

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,10 +3,14 @@ const User = require("../models/user");
 exports.viewProfile = async function(req, res) {
   const user = await User.query()
     .where("username", req.params.username)
-    .first();
+    .first()
+    .eager("review");
   const title = req.params.username + "'s Profile | JetFree by JainsBret";
+  res.json(user);
+  /*
   res.render("profile", {
     title: title,
     user: user
   });
+  */
 };

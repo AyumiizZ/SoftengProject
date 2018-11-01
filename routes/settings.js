752d31c876d426e66fb5e2e39752753c15e4763a
Rawissara <rawissara.r@ku.th>
2018-11-02 02:41:18 +0700

    Add route to changing password page

--- routes/settings.js
+++ routes/settings.js
@@ -24,49 +24,68 @@ router.get("/profile", async function(req, res) {
 });
 
 router.post("/profile", async function(req, res, next) {
-  var pass = true; 
   let user = await User.query()
     .where("username", req.user.username)
-    .first()
+    .first();
   req
     .checkBody("email", "E-mail is not in a valid format.")
     .isEmail();
-  if(req.body.password) {
-    req
-      .checkBody("password", "Password must be at least 8 characters.")
-      .isLength({ min: 8 });
-    req
-      .checkBody("confirm", "Password does not match the confirmation")
-      .equals(req.body.confirm);
-    pass = _helpers.comparePassSync(req.body.old_password, user.password);
+  
+  var errors = req.validationErrors();
+  if (errors) {
+    console.log("error!");
+    res.render("editProfile", {errors: errors, user: user});
+  } else {
+    let updatedUser = await User.query().updateAndFetchById(user.id, req.body);
+    res.redirect("/settings/profile");
   }
+});
+
+router.get("/password", async function(req, res) {
+  let user = await User.query()
+    .where("username", req.user.username)
+    .first();
+  var errors = req.validationErrors();
+  res.render("changePassword", {user: user});
+});
+
+router.post("/password", async function(req, res, next) {
+  var pass = true; 
+  let user = await User.query()
+    .where("username", req.user.username)
+    .first();
+  req
+    .checkBody("password", "Password must be at least 8 characters.")
+    .isLength({ min: 8 });
+  req
+    .checkBody("confirm", "Password does not match the confirmation")
+    .equals(req.body.password);
+  pass = _helpers.comparePassSync(req.body.old_password, user.password);
+
+  console.log(pass);
+
   var errors = req.validationErrors();
   if (errors || !pass) {
     console.log("error!");
     let failed = !pass;
-    res.render("editProfile", {errors: errors, failed: failed, user: user});
+    res.render("changePassword", {errors: errors, failed: failed, user: user});
   } else {
     delete req.body.old_password;
     delete req.body.confirm;
     
-    if(!req.body.password) {
-      delete req.body.password;
-      let updatedUser = await User.query().updateAndFetchById(user.id, req.body);
-      res.redirect("/profile/");
-    } else {
-      _helpers
-        .hashPass(req.body.password)
-        .then(hashed => {
-          console.log(hashed);
-          req.body.password = hashed;
-          return User.query().updateAndFetchById(user.id, req.body);
-        })
-        .then(updatedUser => {
-          res.redirect("/profile/");
-        });
-    }
+    _helpers
+      .hashPass(req.body.password)
+      .then(hashed => {
+        console.log(hashed);
+        req.body.password = hashed;
+        return User.query().updateAndFetchById(user.id, req.body);
+      })
+      .then(updatedUser => {
+        res.redirect("/profile/");
+      });
   }
 });
+
 /*=======
 router.get("/profile", async function(req, res) {
   let user = await User.query()

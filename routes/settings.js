e5c33ae120a80fdd85bae0dc49a4542185bcbfaa
Rawissara <rawissara.r@ku.th>
2018-11-02 20:52:36 +0700

    Add account setting

--- routes/settings.js
+++ routes/settings.js
@@ -27,6 +27,25 @@ router.post("/profile", async function(req, res, next) {
   let user = await User.query()
     .where("username", req.user.username)
     .first();
+  let updatedUser = await User.query().updateAndFetchById(user.id, req.body);
+  res.redirect("/settings/profile");
+});
+
+router.get("/account", async function(req, res) {
+  let user = await User.query()
+    .where("username", req.user.username)
+    .first();
+  var errors = req.validationErrors();
+  res.render("editAccount", {user: user});
+});
+
+router.post("/account", async function(req, res, next) {
+  let user = await User.query()
+    .where("username", req.user.username)
+    .first();
+  req
+    .checkBody("username", "Username must not be empty.")
+    .notEmpty();
   req
     .checkBody("email", "E-mail is not in a valid format.")
     .isEmail();
@@ -34,10 +53,10 @@ router.post("/profile", async function(req, res, next) {
   var errors = req.validationErrors();
   if (errors) {
     console.log("error!");
-    res.render("editProfile", {errors: errors, user: user});
+    res.render("editAccount", {errors: errors, user: user});
   } else {
     let updatedUser = await User.query().updateAndFetchById(user.id, req.body);
-    res.redirect("/settings/profile");
+    res.redirect("/settings/account");
   }
 });
 

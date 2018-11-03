27e63ac77fdfeac1724b5b73d53940bd1d3a2fbe
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-03 12:06:42 +0700

    :sparkle: Better markdown for profile page

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,13 +1,11 @@
 const User = require("../models/user");
 const gravatar = require("gravatar");
-var md = require("markdown-it")();
 
 exports.viewProfile = async function(req, res) {
   const user = await User.query()
     .where("username", req.params.username)
     .first()
     .eager("review");
-  user.overview = md.render(user.overview);
   const title = req.params.username + "'s Profile | JetFree by JainsBret";
   res.render("profile/profile", {
     title: title,

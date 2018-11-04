1f9b5ef76b22e08a1228002d97f4b5da70c1be05
AyumiizZ <korrawitc@gmail.com>
2018-11-04 09:37:05 +0700

    :sparkles: add json file for query

--- public/scripts/job_scripts.js
+++ public/scripts/job_scripts.js
@@ -1,35 +1,30 @@
 $(document).ready(function () {
-    // var skill_len = 0
-    
-    // setInterval(() => {
-    //     if ($("#fix").is(":checked")) {
-    //         console.log("fix checked")
-    //     } else {
-    //         console.log("fix not checked")
-    //     }
-
-    //     if ($("#hour").is(":checked")) {
-    //         console.log("hour checked")
-    //     } else {
-    //         console.log("hour not checked")
-    //     }
-    // }, 100);
-
     setInterval(() => {
-        var skills = $('#skills .input-tag') ;
+        var skills = $('#skills .input-tag');
         var langs = $('#langs .input-tag');
         var skill_arr = [];
         var lang_arr = [];
-        for (i=0;i<skills.length;i++){
-            skill_arr.push(skills[i].innerText.substring(0,skills[i].innerText.length-1));
+        var fix = $("#fix").is(":checked");
+        var hour = $("#hour").is(":checked")
+        for (i = 0; i < skills.length; i++) {
+            skill_arr.push(skills[i].innerText.substring(0, skills[i].innerText.length - 1));
         }
-        for (i=0;i<langs.length;i++){
-            lang_arr.push(langs[i].innerText.substring(0,langs[i].innerText.length-1));
+        for (i = 0; i < langs.length; i++) {
+            lang_arr.push(langs[i].innerText.substring(0, langs[i].innerText.length - 1));
         }
-        console.log(skill_arr);
-        console.log(lang_arr);
-        var j = JSON.stringify(skills)
-        console.log(j)
+        var ret = {
+            'fix': fix,
+            'hour': hour,
+            'skills': skill_arr,
+            'langs': lang_arr,
+            'min_fix': 0,
+            'max_fix': 1000000,
+            'min_hour': 0,
+            'max_hour': 10000,
+            'sort': 'date'
+        }
+        var json = JSON.stringify(ret)
+        console.log(json)
     }, 100);
 
 });

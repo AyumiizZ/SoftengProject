9590dc932304b8ffec975a2ac875d9755adceed5
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-10 14:47:11 +0700

    :rewind: Bringing back MySQL support

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,26 @@
+require("dotenv").config({ path: ".env" });
+
 module.exports = {
   development: {
     client: "sqlite",
     useNullAsDefault: true,
     connection: {
       filename: "./database/dev.sqlite"
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+  mysql: {
+    client: "mysql",
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
     },
     migrations: {
       directory: "./database/migrations"

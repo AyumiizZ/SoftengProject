965d3239e031fc54fccf4b90ea7dbbc4e5585541
AyumiizZ <korrawitc@gmail.com>
2018-11-12 14:46:09 +0700

    :sparkles: add remove tag

--- public/scripts/job_scripts.js
+++ public/scripts/job_scripts.js
@@ -1,11 +1,16 @@
 // $(document).ready(function () {
-
-
+//   $('lang_input').keypress(function (e) {
+//     if (e.which == '13') {
+//         alert('code');
+//     }
+//   });
 // });
 
 function sent_query() {
   var skills = $('#skills .input-tag');
   var langs = $('#langs .input-tag');
+  var dropbtn = $('#sort_by');
+  var sort = dropbtn[0].innerText.substring(8);
   var skill_arr = [];
   var lang_arr = [];
   var fix = $("#fix").is(":checked");
@@ -25,22 +30,57 @@ function sent_query() {
     'max_fix': 1000000,
     'min_hour': 0,
     'max_hour': 10000,
-    'sort': 'date'
+    'sort': sort
   }
   var ret_json = JSON.stringify(ret)
   console.log(ret_json)
 }
 
-function delete_tag(element){
-  console.log(element.parentNode)
+function delete_tag(element) {
+  // console.log(element.parentNode)
   element.parentNode.remove()
   sent_query()
 }
 
-function delete_all_tag(id){
-  var tag_box = $('#'+id+' .input-tag')
+function delete_all_tag(id) {
+  var tag_box = $('#' + id + ' .input-tag')
   for (i = 0; i < tag_box.length; i++) {
     tag_box[i].remove()
   }
   sent_query()
 }
+
+function change_sort(element) {
+  var dropbtn = $('#sort_by');
+  dropbtn[0].innerText = "Sort By " + element.innerText
+  sent_query()
+}
+
+var lang_input = document.getElementById("lang_input");
+
+lang_input.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    if (lang_input.value != '') {
+      var langs = $('#langs .input-tag');
+      var tag = "<div class='input-tag'>" + lang_input.value + "<div class='delete-tag' onclick='delete_tag(this)'>×</div></div>"
+      $(tag).insertAfter(langs[langs.length - 1])
+      lang_input.value = ''
+      sent_query()
+    }
+  }
+});
+
+var skill_input = document.getElementById("skill_input");
+skill_input.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    var skills = $('#skills .input-tag');
+    if (skill_input.value != '') {
+      var tag = "<div class='input-tag'>" + skill_input.value + "<div class='delete-tag' onclick='delete_tag(this)'>×</div></div>"
+      $(tag).insertAfter(skills[skills.length - 1])
+      skill_input.value = ''
+      sent_query()
+    }
+  }
+});

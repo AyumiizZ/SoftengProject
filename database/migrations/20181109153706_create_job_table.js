d556bd80c036189cb1737f46c56d985774bd5d5c
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-17 11:47:46 +0700

    Jobs tags table migration

--- database/migrations/20181109153706_create_job_table.js
+++ database/migrations/20181109153706_create_job_table.js
@@ -4,25 +4,17 @@ exports.up = function(knex, Promise) {
       table.increments("id").primary();
       table.string("job");
       table.string("job_info");
-      table
-        .timestamp('created_at')
-        .defaultTo(knex.fn.now());
-      table
-        .boolean("done")
-        .defaultTo(false);
-      table
-        .integer("client_id")
-        .references("users_id");
-      table
-        .integer("user_id")
-        .references("users_id");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.boolean("done").defaultTo(false);
+      table.integer("client_id");
+      table.integer("user_id");
       table.string("job_type");
       table.string("tag");
       table.integer("price");
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-return knex.schema.dropTable("jobs");
+  return knex.schema.dropTable("jobs");
 };

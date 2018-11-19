2674d6a566489486c55703b40c6562e35434a9ba
Rawissara <rawissara.r@ku.th>
2018-11-20 01:11:40 +0700

    Add pagination to view past jobs

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -6,13 +6,7 @@ exports.viewProfile = async function(req, res) {
   const user = await User.query()
     .where("username", req.params.username)
     .first();
-  var past_job = await Job.query()
-    .where("user_id", user.id)
-    .where("done", 1);
-  let amount = past_job.length;
-  if (past_job.length > 5) {
-    past_job = past_job.slice(0, 5);
-  }
+
   const title = req.params.username + "'s Profile | JetFree by JainsBret";
   res.render("profile/profile", {
     title: title,
@@ -51,14 +45,24 @@ exports.viewPastJobs = async function(req, res) {
   var past_job = await Job.query()
     .where("user_id", user.id)
     .where("done", 1);
+  
+  const amount = past_job.length;
+  const per_page = 5;
+  const page = Number(req.params.page);
+  var jobs = past_job.slice(per_page*(page-1), page*per_page);
+  console.log(jobs);
+
   const title = req.params.username + "'s Past Jobs | JetFree by JainsBret";
   user.gravatar_url = gravatar.url(user.email, { s: 400 });
 
   res.render("profile/pastJobs", {
     user: user,
-    past_job: past_job,
+    past_job: jobs,
     current: req.user,
-    title: title
+    title: title,
+    page: page,
+    n: 1,
+    limit: Math.ceil(amount/per_page)
   });
 };
 

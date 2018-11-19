ad3c2208b52376a104f7c8d06cd8cf85485d5c5d
AyumiizZ <korrawitc@gmail.com>
2018-11-19 11:00:21 +0700

    :sparkles: simplify get_tag

--- public/scripts/job_scripts.js
+++ public/scripts/job_scripts.js
@@ -1,18 +1,20 @@
+function get_tag(id){
+  var arr = [];
+  var inputs = $('#'+id+' .input-tag');
+  for (i = 0; i < inputs.length; i++) {
+    arr.push(inputs[i].innerText.substring(0, inputs[i].innerText.length - 1));
+  }
+  return arr
+}
+
+
 function sent_query() {
-  var skills = $('#skills .input-tag');
-  var langs = $('#langs .input-tag');
-  var dropbtn = $('#sort_by');
+  var dropbtn = $('#sort-by');
   var sort = dropbtn[0].innerText.substring(8);
-  var skill_arr = [];
-  var lang_arr = [];
+  var skill_arr = get_tag('skills');
+  var lang_arr = get_tag('langs');
   var fix = $("#fix").is(":checked");
   var hour = $("#hour").is(":checked")
-  for (i = 0; i < skills.length; i++) {
-    skill_arr.push(skills[i].innerText.substring(0, skills[i].innerText.length - 1));
-  }
-  for (i = 0; i < langs.length; i++) {
-    lang_arr.push(langs[i].innerText.substring(0, langs[i].innerText.length - 1));
-  }
   var ret = {
     'fix': fix,
     'hour': hour,
@@ -29,7 +31,6 @@ function sent_query() {
 }
 
 function delete_tag(element) {
-  // console.log(element.parentNode)
   element.parentNode.remove()
   sent_query()
 }
@@ -48,15 +49,26 @@ function change_sort(element) {
   sent_query()
 }
 
+function choose_this(){
+  sent_query()
+}
+
+
 var lang_input = document.getElementById("lang-input");
 
 lang_input.addEventListener("keyup", function (event) {
   event.preventDefault();
   if (event.keyCode === 13) {
     if (lang_input.value != '') {
-      var langs = $('#langs .input-tag');
       var tag = "<div class='input-tag'>" + lang_input.value + "<div class='delete-tag' onclick='delete_tag(this)'>×</div></div>"
-      $(tag).insertAfter(langs[langs.length - 1])
+      var langs = $('#langs .input-tag');
+      if(langs.length > 0)
+        $(tag).insertAfter(langs[langs.length - 1])
+      else{
+        var lang = $('#langs #lang-input');
+        $(tag).insertBefore(lang);
+      }
+      // console.log(langs.innerHTML)
       lang_input.value = ''
       sent_query()
     }

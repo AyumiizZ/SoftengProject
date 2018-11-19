9b638e5a0b395024b50b6fd4c2c59b4a0e83913e
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-11-19 10:33:30 +0700

    Job boosts migration table

---
+++ database/migrations/20181119102938_create_job_boosts_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("jobs_boosts", table => {
+    table.increments("id");
+    table.integer("job_id");
+    table.date("start");
+    table.date("end");
+    table.date("price");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("jobs_boosts");
+};

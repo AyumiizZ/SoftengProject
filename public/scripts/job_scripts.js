afff95db01d958dcb6743539ebd70d61c6e17b77
AyumiizZ <korrawitc@gmail.com>
2018-12-02 09:31:12 +0700

    :sparkles: add template html

--- public/scripts/job_scripts.js
+++ public/scripts/job_scripts.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-  // var sent_query = $(function () {
-  //   var data = get_query()
-  //   $.ajax({
-  //     type: "POST",
-  //     data: data,
-  //     url: "/jobs/browse",
-  //     contentType: "application/json"
-  //   });
-  // });
 
   var sent_query = function () {
     $(function () {
@@ -25,10 +16,50 @@ $(document).ready(function () {
 
   var render = function(data){
     console.log(data)
-    $(".search-result-list").html("");
-    for(var i=0; i<data.length; i++){
-      $(".search-result-list").append(data[i].title);
+    console.log($(".search-result-list"))
+    $(".result-amount").html(data.length + " Result")
+    // $(".search-result-list").html("");
+    res = ""
+    for(i = 0; i < data.length;i++){
+      res += `<div class="search-result-item">
+      <div class="project-tile">
+        <figure class="info-card-iconBox"><span class="Icon">
+            <fl-icon name="ui-fixed-project"><svg class="Icon-image" width="24" height="24" xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <path d="M0 15.5v1c0 1.103.897 2 2 2h9v2H7v2h10v-2h-4v-2h9c1.103 0 2-.897 2-2v-1H0zm24-1v-11c0-1.103-.897-2-2-2H2c-1.103 0-2 .897-2 2v11h24z"
+                  fill="#0087E0"></path>
+              </svg></fl-icon>
+          </span></figure>
+        <div class="info-card-inner">
+          <h2 class="info-card-title">Build me a website</h2>
+          <p class="info-card-description">I need website for my upcoming project For that I need expert freelancer</p>
+          <div class="info-card-grid">
+            <div class="info-card-details info-card-grid-item"><img src="/svgs/solid/hourglass-start.svg" width="16" height="16"
+                alt=""><time>2018-12-02 01:45:42</time></div>
+            <div class="info-card-details info-card-grid-item"><img src="/svgs/solid/user.svg" width="16" height="16" alt=""><span>JetFree</span></div>
+            <div class="info-card-details info-card-grid-item info-card-skills-container"><img src="/svgs/solid/tags.svg"
+                width="16" height="16" alt="">
+              <div class="btn btn-outline-secondary btn-sm" style="padding: 0px 3px; margin-right: 3px">Graphic Design</div>
+              <div class="btn btn-outline-secondary btn-sm" style="padding: 0px 3px; margin-right: 3px">HTML</div>
+              <div class="btn btn-outline-secondary btn-sm" style="padding: 0px 3px; margin-right: 3px">PHP</div>
+              <div class="btn btn-outline-secondary btn-sm" style="padding: 0px 3px; margin-right: 3px">Website Design</div>
+              <div class="btn btn-outline-secondary btn-sm" style="padding: 0px 3px; margin-right: 3px">WordPress</div><span></span>
+            </div>
+          </div>
+        </div>
+        <div class="info-card-rate">
+          <div class="info-card-price"><span>7500</span></div>
+          <div class="info-card-price-type"><span>THB</span></div>
+        </div>
+      </div>
+    </div>`
     }
+    $(".search-result-list").html(res);
+    // $(".search-result-list")[0].append("<li>");
+    // for(var i=0; i<data.length; i++){
+    //   $(".search-result-list")[0].append(data[i].job);
+    // }
+    // $(".search-result-list")[0].append("</li>");
   }
 
   var delete_tag = function () {
@@ -101,8 +132,6 @@ $(document).ready(function () {
       });
     });
   }
-
-  // console.log($('#min,#max'))
 
   $('#min-fix,#max-fix,#min-hour,#max-hour').on('keyup', sent_query);
 

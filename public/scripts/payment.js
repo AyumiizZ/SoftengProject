9ef32e61ce4f05e31458a3eb124a8cc8c0c1fe43
Sirakorn Lamyai <tanersirakorn@gmail.com>
2018-12-02 16:01:05 +0700

    Client side form validation

--- public/scripts/payment.js
+++ public/scripts/payment.js
@@ -8,3 +8,50 @@ $("#expire").mask("00/00", {
   placeholder: "MM/YY"
 });
 $('[data-toggle="popover"]').popover();
+
+$("#cardnumber").keyup(function() {
+  var number = $("#cardnumber").cleanVal();
+  var numberValidation = valid.number(number);
+  $("#credit-card-icon").removeClass();
+  if (numberValidation.card != null) {
+    var cardType = numberValidation.card.type;
+    if (cardType == "visa") {
+      $("#credit-card-icon")
+        .addClass("fab")
+        .addClass("fa-cc-visa");
+    } else if (cardType == "mastercard") {
+      $("#credit-card-icon")
+        .addClass("fab")
+        .addClass("fa-cc-mastercard");
+    } else if (cardType == "jcb") {
+      $("#credit-card-icon")
+        .addClass("fab")
+        .addClass("fa-cc-jcb");
+    } else {
+      $("#credit-card-icon")
+        .addClass("far")
+        .addClass("fa-credit-card");
+    }
+  } else {
+    $("#credit-card-icon")
+      .addClass("far")
+      .addClass("fa-credit-card");
+  }
+  $("#credit-card-icon").addClass("fa-2x");
+});
+
+$("#cardnumber").change(function() {
+  var number = $("#cardnumber").cleanVal();
+  var numberValidation = valid.number(number);
+  if (numberValidation.isValid) {
+    $("#card-form button").attr("disabled", false);
+    $("#cardnumber")
+      .removeClass("is-invalid")
+      .addClass("is-valid");
+  } else {
+    $("#card-form button").attr("disabled", true);
+    $("#cardnumber")
+      .addClass("is-invalid")
+      .removeClass("is-valid");
+  }
+});
